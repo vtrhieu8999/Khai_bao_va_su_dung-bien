79da41f9f9b939e8b6defb55741ee41b1401a8ca
Vuong_Trung_Hieu <v.hieu1998@gmail.com>
2020-09-10 11:32:55 +0700

    Khai bao va su dung bien

---
+++ src/Khai_bao_va_su_dung_bien.java
@@ -0,0 +1,15 @@
+
+import java.lang.String;
+
+public class Khai_bao_va_su_dung_bien {
+    public static void main(String[] agrs){
+        int i= 10;
+        float j= 20.5f;
+        double d= 20.5;
+        boolean b = true;
+        char c= 'a';
+        String s=  "Ha Noi";
+        System.out.println("Bien i la kieu so nguyen, co gia tri la"+ i + "\n Bien j la kieu so thuc, co gia tri la "+j+" \n Bien d la kieu so thuc dai, co gia tri la "+d+" \n Bien b la kieu logic, co gia tri la "+b+" \n Bien c la kieu ky tu, co gia tri la "+c+" \n Bien s la kieu chuoi ky tu, co gia tri la "+s);
+    }
+
+}
